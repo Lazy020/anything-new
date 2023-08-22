f5d1c6e2e78705ce3e67e5cb9f257482d8772c8d
Ana <104568665+Lazy020@users.noreply.github.com>
2023-08-22 12:59:40 +0000

    things

--- calculadora/src/App.tsx
+++ calculadora/src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/calc.png'
 
-function App() {
-  const [count, setCount] = useState(0)
+/*
+Calculo: alccol/gasolina
+Se resultado menor 0.7 comepnsa Álcool */
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface resultadoP{
+  titulo: string;
+  gasolina: string | number;
+  alcool: string | number;
 }
+ function App() {
+  const [gasolinaInput, setGasolinaInput] = useState(0)
+  const [alcoolInput, setAlcoolInput] = useState(0)
+  const [resultado, setResultado] = useState <resultadoP>()
+
+  function calculo(event: FormEvent) {
+    event.preventDefault();
+    let conta = (alcoolInput/gasolinaInput)
+    console.log(conta)
+  if(conta <= 0.7){
+    setResultado({
+      titulo:"Compensa usar Álcool",
+      gasolina: formataMoeda(gasolinaInput),
+      alcool: formataMoeda(alcoolInput)
+    })
+  }else {
+      setResultado({
+        titulo:"Compensa usar Gasolina",
+        gasolina: formataMoeda(gasolinaInput),
+        alcool: formataMoeda(alcoolInput)
+      })
+    }
+  }
+  function formataMoeda(valor:number){
+    let valorFormatado = valor.toLocaleString(pt-br,
+    {
+      style: "currency",
+      currency: "BRL"
+    })
+    return valorFormatado;
+    }
+  }
+ 
+
+
+
 
 export default App
